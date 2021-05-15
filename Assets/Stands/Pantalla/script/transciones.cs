6643f7244ab34fc2c49f3c418b4c41f3548517ef
carcinos93 <nelson.rodas1993@gmail.com>
2021-05-15 16:56:23 -0600

    cambios

--- Assets/Stands/Pantalla/script/transciones.cs
+++ Assets/Stands/Pantalla/script/transciones.cs
@@ -5,10 +5,9 @@ using System.Collections;
 public class transciones : MonoBehaviour {
 
 public void CambiarEscena(string nombre){
-
     print("Cambiando a la escena " + nombre);
     Application.ExternalCall("externalCall", nombre);
    // SceneManager.LoadScene(nombre);
-}   
+    }   
    
 }
